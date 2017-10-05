2774c0c9abae331ba77a5bbd0b41daadf0c9b87d
pikepeak <khunach@msn.com>
2017-10-05 13:28:31 +0700

    more prototype

--- app/src/main/java/kmitl/lab05/topusenpai/newkmitlapp/Fragment/MoreFragment.java
+++ app/src/main/java/kmitl/lab05/topusenpai/newkmitlapp/Fragment/MoreFragment.java
@@ -6,14 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import kmitl.lab05.topusenpai.newkmitlapp.CustomAdapter;
 import kmitl.lab05.topusenpai.newkmitlapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MoreFragment extends Fragment {
-
     public static MoreFragment newInstance() {
         
         Bundle args = new Bundle();
@@ -30,8 +36,26 @@ public class MoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        View v = inflater.inflate(R.layout.fragment_more, container, false);
+        int[] resId = { R.mipmap.micro
+                , R.mipmap.shop, R.mipmap.student
+                , R.mipmap.book, R.mipmap.feedback
+                , R.mipmap.photo};
+
+        String[] list = { "Free Microsoft Office", "KMITL Promotion", "Student Benefit"
+                , "Contact", "Feedback & Report", "KMITL Sharing Photo"};
+
+        CustomAdapter adapter = new CustomAdapter(getContext(), list, resId);
+
+        ListView listView = (ListView)v.findViewById(R.id.listView1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+            }
+        });
+        return v;
     }
+
 
 }
